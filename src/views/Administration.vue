14168d2be98673a24f63e3ff08f920aca618f82f
Adriel da Silva <silvaadriel510@gmail.com>
2020-01-26 22:57:06 -0300

    Create admin tabs and user CRUD

--- src/views/Administration.vue
+++ src/views/Administration.vue
@@ -5,16 +5,35 @@
       mainTitle="Administration"
       subtitle="Registrations and more."
     />
+    <b-card no-body>
+      <b-tabs card>
+        <b-tab title="Articles" active>
+          <base-article-admin />
+        </b-tab>
+        <b-tab title="Categories">
+          <base-category-admin />
+        </b-tab>
+        <b-tab title="Users">
+          <base-user-admin />
+        </b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
 <script>
 import BasePageTitle from '../components/BasePageTitle.vue';
+import BaseUserAdmin from '../components/BaseUserAdmin.vue';
+import BaseArticleAdmin from '../components/BaseArticleAdmin.vue';
+import BaseCategoryAdmin from '../components/BaseCategoryAdmin.vue';
 
 export default {
   name: 'Administration',
   components: {
     BasePageTitle,
+    BaseUserAdmin,
+    BaseArticleAdmin,
+    BaseCategoryAdmin,
   },
 };
 </script>
